d1fb00793f100849291e057ae4ccf5c3eeb35473
LZM08 <lizhiming1008@gmail.com>
2024-07-18 13:24:22 +0900

    test

--- Practice/test.py
+++ Practice/test.py
@@ -1,1 +1,22 @@
-
+import requests
+from bs4 import BeautifulSoup
+ 
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.melon.com/chart/',headers=headers)
+ 
+soup = BeautifulSoup(data.text, 'html.parser')
+ 
+top_100 = soup.select('#lst50 > td > div') #tr로 이루어진 list
+ 
+for tr in top_100 :
+    rank = tr.select_one('span.rank')
+        
+    if rank is not None:
+        print(rank.text, end=". ")
+        
+    title = tr.select_one('div > div.ellipsis.rank01 > span > a')
+    
+    if title is not None:
+        artist = tr.select_one('div.ellipsis.rank02 > a').text
+        print(artist,'-', title.text)
+#lst50 > td:nth-child(6) > div > div > div.ellipsis.rank01 > span > a
