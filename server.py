f5401fada2adf480f4efb498d8c8951f2ff665ab
LZM08 <lizhiming1008@gmail.com>
2024-07-22 00:17:47 +0900

    12:17

--- server.py
+++ server.py
@@ -10,38 +10,54 @@ topics = [
     
 ]
 
-
-
-
-
-@app.route('/')
-def index():
-    liTags = ''
-    for topic in topics:
-        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic['title']}</a></li>'
+def template(contents, content):
     return f'''
 <!doctype html>
 <html>
     <body>
         <h1><a href="/">WEB</a></h1>
         <ol>
-            {liTags}
+            {contents}
         </ol>
-        <h2>Welcome</h2>
+        {content}
         hello, web
     </body>
 </html>
 '''
 
 
+def getContents():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return liTags
+    
+
+
+
+@app.route('/')
+def index():
+    return template(getContents(), '<h2>Welcome</h2>Hello, WEB')
+
 
 @app.route('/create/')
 def create():
     return 'Create'
 
-@app.route('/read/<id>/')
+
+
+@app.route('/read/<int:id>/')
 def read(id):
-    return 'read' + id
+    title =''
+    body = ''
+    for topic in topics:
+        if id == topic['id']:
+            title= topic['title']
+            body = topic['body']
+    
+    return template(getContents(), f'<h2>{title}</h2>{body}')
+
+
 
 
 app.run(port = 5001,debug=True)
